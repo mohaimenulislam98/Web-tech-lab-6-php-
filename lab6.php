6e54f441b9ba560827b6923749df743dc63eef88
Mohammed Mohaimenul Islam <73056826+mohaimenulislam98@users.noreply.github.com>
2021-03-24 10:10:01 +0600

    Add files via upload

---
+++ lab6.php
@@ -0,0 +1,26 @@
+<html>    
+    <head>    
+        <title>ADD PRODUCT</title>    
+    </head>    
+    <body>      
+        <h2>ADD PRODUCT</h2>    
+        <form >        
+                    <label>Name:</label> 
+                    <br>   
+                    <input type = "text" name = "fname" value = "" required/>
+                    <br>       
+                    <label>Buying Price</label> 
+                    <br>   
+                    <input type = "number" name = "mnu," value = "" required />
+                    <br>
+                    <label>Selling Price</label> 
+                    <br>   
+                    <input type = "number" name = "nnum" value = "" required/> 
+                    <br><br>
+                    <input type="checkbox" checked="checked" name="remember"> Display
+                    <br><br>
+                    <input type="submit" name="SAVE" value="Submit" />   
+              
+        </form>    
+    </body>    
+</html> 
